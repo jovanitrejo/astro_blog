665a90db618a56a9022b008434003d0c3d08976d
Jovani Trejo <jovanitrejo@icloud.com>
2026-03-15 19:04:57 -0500

    Updated config, added custom 404, and about me

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [mdx()],
     prefetch: true,
+    image: {
+        responsiveStyles: true,
+    }
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { z } from 'astro/zod';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/blog' }),
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/blog' }),
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
